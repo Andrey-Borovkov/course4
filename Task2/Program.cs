6e0ae6f36e6d1a41833631d7dc141f5d2d83f15c
Your Name01~ <you@example.com>
2022-03-01 10:52:58 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 == Number2) 
+    Console.WriteLine("Числа равны");
+
+else
+
+    if (Number1 > Number2) 
+        
+            Console.WriteLine("Первое число больше вторго");
+        
+    else
+            Console.WriteLine("Второе число больше первого");
+
