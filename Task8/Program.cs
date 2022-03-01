f84fccbabf8d40ee4bacf57e9ec15a246a52c8e7
Your Name01~ <you@example.com>
2022-03-01 15:17:46 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+if (number >= 2) 
+{
+    while (index <= number)
+    {
+        Console.WriteLine(index);
+        index = index + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число меньше 2");
+}
