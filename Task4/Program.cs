af1eaaa8deae9f99758c6925d0a8516dae22b084
Your Name01~ <you@example.com>
2022-03-01 14:48:55 +0300

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите три числа");
+
+Console.Write("1: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("2: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("3: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = Number1;
+if (Number2 > max) max = Number2;
+if (Number3 > max) max = Number3;
+
+Console.Write(max);
+Console.Write(" является максимальным среди остальных чисел");
+//Console.Write(max);
