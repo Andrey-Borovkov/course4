38cf074646cbef3f490db1c24519be3390c3de0d
Andrey <borov_a@list.ru>
2022-03-21 19:20:29 +0300

    modify task17

---
+++ Task17/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Выберете четверть: ");
+int Chetvert = Convert.ToInt32(Console.ReadLine());
+if (Chetvert == 1)
+{
+    Console.WriteLine("Координаты X и Y могут принимать значения больше 0");
+}
+else
+{
+    if (Chetvert == 2)
+    {
+       Console.WriteLine("Координаты X  могут принимать отрицательные значения, а координаты Y  больше 0");
+    } 
+    else
+    {
+      if (Chetvert == 3)
+    {
+       Console.WriteLine("Координаты X и Y  могут принимать отрицательные значения");
+    }  
+    else
+    {
+       Console.WriteLine("Координаты X  могут принимать положительные значения, а координаты Y  отрицательные"); 
+    } 
+    }   
+    
+}
