8d2c66e7d35433b2db11694761708d61157dc2ce
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-03-28 09:30:07 +0300

    Add Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+void CreateArray (double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(-100, 101);
+    }
+}
+
+void ShowArray(double [] array)
+{
+        Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+double DiffOfNumbers(double [] array)
+{
+    double result = new double();
+    double MinOfNumbers = array[0];
+    double MaxOfNumbers = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > MaxOfNumbers)
+        {
+            MaxOfNumbers = array[i];
+        }
+        else
+        {
+            if (array[i] < MinOfNumbers)
+            {
+                MinOfNumbers = array[i];
+            }
+        }
+    }
+    result = MaxOfNumbers - MinOfNumbers;
+    return result;
+}
+
+Console.WriteLine("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double [] arr = new double [size];
+CreateArray(arr);
+ShowArray(arr);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {DiffOfNumbers(arr)}");
