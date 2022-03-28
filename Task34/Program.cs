030c8235d436b9b1ba8877724ed65bf1c104dcd9
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-03-28 07:50:58 +0300

    Add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+void CreateArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(100, 1000);
+    }
+}
+
+void ShowArray(int[] array)
+{
+        Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+int ChekCount(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] % 2) == 0) 
+        {
+            count++;
+        } 
+    }
+    return count;
+}
+Console.WriteLine("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [size];
+CreateArray(arr);
+ShowArray(arr);
+Console.WriteLine($"Количество четных чисел в массиве равно {ChekCount(arr)}");
