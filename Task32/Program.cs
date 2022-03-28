389128203cd61fcd2fd118b11deeec112f149fc8
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-03-28 10:50:27 +0300

    Add Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+void CreateArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(-100, 101);
+    }
+}
+
+void ShowArray(int [] array)
+{
+        Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+int [] Replace(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+    return array;
+}
+
+Console.WriteLine("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [size];
+CreateArray(arr);
+ShowArray(arr);
+Replace(arr);
+ShowArray(arr);
