0ec834a04795a7f8ff91683142bd87097c8a7e36
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-04-01 09:42:33 +0300

    Add Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+void Linedoubleersaction(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = 5 * x + 2;
+    Console.WriteLine($"При введенных значениях координаты точки пересечения равны соответственно x = {x} и y = {y}");
+}
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Linedoubleersaction(k1, b1, k2, b2);
