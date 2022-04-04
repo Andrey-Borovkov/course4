5913b8f9541873841f81abf64088d32ecd168e01
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-04-04 11:57:13 +0300

    Add Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть
+вот так:
+1 4 7 2
+5 81 2 9
+8 4 2 4
+*/
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] Array = new int[rows, columns];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return Array;
+}
+void PrintArray(int[,] PrintedArray)
+{
+    for (int i = 0; i < PrintedArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < PrintedArray.GetLength(1); j++)
+        {
+            Console.Write($"{PrintedArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] ChangedArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = (int) Math.Pow(array[i, j], 2);
+            }
+        }
+    }
+    return array;
+}
+
+Console.Write("Введите количество строк массива: ");
+int numbersOfLines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int numbersOfColumns = Convert.ToInt32(Console.ReadLine());
+int[,] Array = CreateArray(numbersOfLines, numbersOfColumns);
+PrintArray(Array);
+Console.WriteLine();
+Console.WriteLine();
+int[,] chengeArray =ChangedArray(Array);
+PrintArray(chengeArray);
