2e112f3d0ce7cf9bb8cfc99313cfb2eb693da15f
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-04-06 08:33:33 +0300

    Add Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+*/
+int[,] Create2DArray(int rows, int columns)
+{
+    int[,] Array = new int[rows, columns];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return Array;
+}
+void Print2DArray(int[,] PrintedArray)
+{
+    for (int i = 0; i < PrintedArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < PrintedArray.GetLength(1); j++)
+        {
+            Console.Write($"{PrintedArray[i, j]} \t ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] ReplaceingRowsWithColumns(int[,] inputArray)
+{
+    int[,] newArray = new int[inputArray.GetLength(0), inputArray.GetLength(1)];
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            newArray[i, j] = inputArray[j, i];
+        }
+    }
+    return newArray;
+}
+Console.Write("Введите количество строк массива: ");
+int numbersOfLines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int numbersOfColumns = Convert.ToInt32(Console.ReadLine());
+int[,] Array = Create2DArray(numbersOfLines, numbersOfColumns);
+Print2DArray(Array);
+Console.WriteLine();
+int[,] newArray = ReplaceingRowsWithColumns(Array);
+Print2DArray(newArray);
