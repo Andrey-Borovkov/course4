cfc24b6ce1eba2b11f819f8914bfc7ee58742e16
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-04-19 08:55:29 +0300

    Add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от N до M.
+    M = 1; N = 5. -> "5, 4, 3, 2, 1"
+    M = 4; N = 8. -> "8, 7, 6, 7"
+*/
+
+void PrintIntegers(int n, int m)
+{
+    if (n<=m)
+    {
+        Console.Write($"{n} ");
+        PrintIntegers(n+1, m);
+    }
+    else
+    {
+        return;
+    }
+}
+PrintIntegers(4, 8);
