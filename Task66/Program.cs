6c2cc93f4aa8bb2f06576a6190e906ad64239591
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-04-19 09:33:45 +0300

    Add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+    Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+    M = 1; N = 15 -> 120
+    M = 4; N = 8. -> 30
+*/
+int SumOfIntegers(int m, int n, int sum = 0)
+{
+    if (m <= n)
+    {
+        sum = m + SumOfIntegers(m + 1, n);
+        return sum;
+    }
+    else
+    {
+        return sum;
+    }
+}
+Console.Write($"Сумма натуральных элементов равна {SumOfIntegers(1, 15)}");
